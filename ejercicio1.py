b458ff5d8e0bef3135e189ae0b6dcb6e49805539
Ronny Lopez <ronnylb25@gmail.com>
2023-11-15 02:13:17 +0000

    subir

--- ejercicio1.py
+++ ejercicio1.py
@@ -2,5 +2,12 @@
 
 #Problema 1
 nombre=str(input("Solicitar nombre: "))
-print("¡Hola <nombre>")
+print("¡Hola",nombre,"!")
 
+#Problema 2
+consumo= float(input("¿Cuanto fue su consumo?: "))
+propina= complex(input("¿Cuanto de propina desea dejar?: "))
+
+propinafinal=consumo*propina
+
+print(propinafinal)
